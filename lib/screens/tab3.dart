28fa4f75338bf283fe85308c4bccf315752b7b93
monkey1soong <77332723+monkey1soong@users.noreply.github.com>
2024-12-27 21:15:21 +0900

    Changed tab3 w/o changing main function -- still need some changed for tab3

--- lib/screens/tab3.dart
+++ lib/screens/tab3.dart
@@ -1,10 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Tab3 extends StatelessWidget {
+class Event {
+  final String title;
+  Event(this.title);
+
+  @override
+  String toString() => title;
+}
+
+class Tab3 extends StatefulWidget {
+  const Tab3({Key? key}) : super(key: key);
+
+  @override
+  _Tab3State createState() => _Tab3State();
+}
+
+class _Tab3State extends State<Tab3> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Map<DateTime, List<Event>> events = {};
+  TextEditingController _eventController = TextEditingController();
+  late final ValueNotifier<List<Event>> _selectedEvents;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _selectedEvents.value = _getEventsForDay(selectedDay);
+      });
+    }
+  }
+
+  void _addEvent(String title) {
+    if (_selectedDay != null && title.isNotEmpty) {
+      setState(() {
+        if (!events.containsKey(_selectedDay)) {
+          events[_selectedDay!] = [];
+        }
+        events[_selectedDay!]!.add(Event(title));
+      });
+      _selectedEvents.value = _getEventsForDay(_selectedDay!);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("This is Tab 3"),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                scrollable: true,
+                title: const Text("Add Event"),
+                content: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _eventController,
+                    decoration: const InputDecoration(
+                      labelText: 'Event Title',
+                    ),
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _addEvent(_eventController.text);
+                      _eventController.clear();
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("ADD"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            locale: 'en_US',
+            firstDay: DateTime.utc(2021, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: _onDaySelected,
+            eventLoader: _getEventsForDay,
+            headerStyle: const HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(value[index].title),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
